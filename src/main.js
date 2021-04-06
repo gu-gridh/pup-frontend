5c9586de13418bc62e19851d22577ddc32db2905
Arild Matsson <arild.matsson@gu.se>
2021-04-06 16:01:06 +0200

    Use columns for gallery, skip Masonry

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { VueMasonryPlugin } from "vue-masonry";
 
 Vue.config.productionTip = false;
-Vue.use(VueMasonryPlugin);
 
 new Vue({
   router,
